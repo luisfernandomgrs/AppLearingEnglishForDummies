f33fd875b5785ccae0bb5d14f397e3ad4af628bb
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-06-22 00:28:18 -0300

    playing sound of numbers, with other mode to play sounds

--- app/src/main/java/com/luisf/learning/learningenglishbasic/Fragments/NumerosFragment.java
+++ app/src/main/java/com/luisf/learning/learningenglishbasic/Fragments/NumerosFragment.java
@@ -1,5 +1,6 @@
 package com.luisf.learning.learningenglishbasic.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.luisf.learning.learningenglishbasic.R;
 
@@ -15,7 +17,9 @@ import com.luisf.learning.learningenglishbasic.R;
  * Use the {@link NumerosFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements View.OnClickListener {
+    private ImageButton numberOne, numberTwo, numberThree, numberFour, numberFive, numberSix;
+    private MediaPlayer mediaPlayer;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +65,61 @@ public class NumerosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        numberOne = view.findViewById(R.id.numberOne);
+        numberTwo = view.findViewById(R.id.numberTwo);
+        numberThree = view.findViewById(R.id.numberThree);
+        numberFour = view.findViewById(R.id.numberFour);
+        numberFive = view.findViewById(R.id.numberFive);
+        numberSix = view.findViewById(R.id.numberSix);
+
+        numberOne.setOnClickListener(this);
+        numberTwo.setOnClickListener(this);
+        numberThree.setOnClickListener(this);
+        numberFour.setOnClickListener(this);
+        numberFive.setOnClickListener(this);
+        numberSix.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.numberOne:
+                this.playSound(R.raw.one);
+                break;
+            case R.id.numberTwo:
+                this.playSound(R.raw.two);
+                break;
+            case R.id.numberThree:
+                this.playSound(R.raw.three);
+                break;
+            case R.id.numberFour:
+                this.playSound(R.raw.four);
+                break;
+            case R.id.numberFive:
+                this.playSound(R.raw.five);
+                break;
+            case R.id.numberSix:
+                this.playSound(R.raw.six);
+                break;
+        }
+    }
+
+    private void playSound(int id) {
+        mediaPlayer = MediaPlayer.create(getActivity(), id);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
     }
 }
